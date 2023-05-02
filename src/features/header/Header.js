75a8e8a0e029d666f99f1aa14d0ca3cfa0616b34
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-02 23:14:42 +0200

    Better search input

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -23,26 +23,35 @@ const Header = () => {
     };
 
     return (
-        <header>
-            <Title />
-            <form className="search" onSubmit={onSearchTermSubmit}>
-                <input
-                    className="peer block w-full border-0 bg-gray-50 py-1.5 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
-                    type="text"
-                    name="search"
-                    id="search"
-                    placeholder="Search"
-                    value={searchValue}
-                    onChange={onSearchTermChange}
-                    aria-label="Search posts"
-                />
-                <button
-                    type="submit"
-                    onClick={onSearchTermSubmit}
-                    aria-label="Search">
-                    <DocumentMagnifyingGlassIcon />
-                </button>
-            </form>
+        <header className="bg-white mt-4">
+            <div className="mx-auto max-w-7xl ml-6">
+                <Title />
+                <form
+                    className="relative max-w-lg mx-auto"
+                    onSubmit={onSearchTermSubmit}>
+                    <label
+                        htmlFor="name"
+                        className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900">
+                        Search
+                    </label>
+                    <input
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        type="text"
+                        name="search"
+                        id="search"
+                        placeholder="search posts"
+                        value={searchValue}
+                        onChange={onSearchTermChange}
+                        aria-label="Search posts"
+                    />
+                    <button
+                        type="submit"
+                        onClick={onSearchTermSubmit}
+                        aria-label="Search">
+                        <DocumentMagnifyingGlassIcon />
+                    </button>
+                </form>
+            </div>
         </header>
     );
 };
