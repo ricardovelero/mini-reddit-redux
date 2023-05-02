bce154c9698d3eb76662493dfadd05ef5665fe94
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-02 23:14:11 +0200

    Better design comment display

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -2,17 +2,40 @@ import React from "react";
 import Avatar from "../avatar/Avatar";
 import moment from "moment";
 import ReactMarkdown from "react-markdown";
+import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/20/solid";
 
 const Comment = ({ comment }) => {
     return (
-        <div className="transition duration-300 ease-in rounded mx-0 my-2 p-4 hover:drop-shadow-xl;">
-            <div className="flex items-center mb-4">
-                <Avatar name={comment.author} />
-                <p className="font-bold text-blue-900 mr-8">{comment.author}</p>
-                <ReactMarkdown className="ml-2">{comment.body}</ReactMarkdown>
-                <p className="italic ml-auto">
-                    {moment.unix(comment.created_utc).fromNow()}
-                </p>
+        <div className="relative pb-8">
+            <div className="relative flex items-start space-x-3">
+                <div className="relative">
+                    <Avatar
+                        name={comment.author}
+                        className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-white"
+                    />
+                    <span className="absolute -bottom-0.5 -right-1 rounded-tl bg-white px-0.5 py-px">
+                        <ChatBubbleLeftEllipsisIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                        />
+                    </span>
+                </div>
+                <div className="min-w-0 flex-1">
+                    <div>
+                        <div className="text-sm">
+                            <p className="font-bold text-blue-900">
+                                {comment.author}
+                            </p>
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">
+                            Commented{" "}
+                            {moment.unix(comment.created_utc).fromNow()}
+                        </p>
+                        <ReactMarkdown className="mt-2 text-sm text-gray-700">
+                            {comment.body}
+                        </ReactMarkdown>
+                    </div>
+                </div>
             </div>
         </div>
     );
