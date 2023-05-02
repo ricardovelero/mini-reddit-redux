9866ee4c7730b69baffca297c95de1b95fe2421e
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-02 23:37:32 +0200

    Add logo

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -2,7 +2,8 @@ import React from "react";
 
 export const Title = () => {
     return (
-        <div className="ml-7 mb-7">
+        <div className="mx-auto mb-7 flex items-end justify-center">
+            <img src="logo.png" alt="logo" width="50" />
             <h1 className="text-3xl font-bold text-center">Mini Reddit App</h1>
         </div>
     );
