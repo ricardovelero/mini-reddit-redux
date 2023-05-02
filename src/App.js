13817189b6cb044e75c61656c19bad4a85d8265f
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-02 08:45:00 +0200

    Add home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./features/header/Header";
+import Home from "./features/Home/Home";
 
 function App() {
     return (
         <>
             <Header />
+            <main>
+                <Home />
+            </main>
         </>
     );
 }
