6af6bb4bf5c7dcb371cbbc8f58b9617a3908b565
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-02 08:46:16 +0200

    Create home component

---
+++ src/features/Home/Home.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AnimatedList } from "react-animated-list";
+import getRandomNumber from "../../utils/getRandomNumber";
+import "./Home.css";
+import Post from "../Post/Posts";
+import PostLoading from "../Post/PostLoading";
+import {
+    fetchPosts,
+    selectFilteredPosts,
+    setSearchTerm,
+    fetchComments,
+} from "../../app/redditSlice";
+
+const Home = () => {
+    const reddit = useSelector((state) => state.reddit);
+    const { isLoading, error, searchTerm, selectedSubreddit } = reddit;
+    const posts = useSelector(selectFilteredPosts);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPosts(selectedSubreddit));
+    }, [selectedSubreddit]);
+
+    const onToggleComments = (index) => {
+        const getComments = (permalink) => {
+            dispatch(fetchComments(index, permalink));
+        };
+
+        return getComments;
+    };
+
+    if (isLoading) {
+        return (
+            <AnimatedList animation="zoom">
+                {Array(getRandomNumber(3, 10)).fill(<PostLoading />)}
+            </AnimatedList>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="error">
+                <h2>Failed to load posts.</h2>
+                <button
+                    type="button"
+                    onClick={() => dispatch(fetchPosts(selectedSubreddit))}>
+                    Try Again
+                </button>
+            </div>
+        );
+    }
+
+    if (posts.length === 0) {
+        return (
+            <div className="error">
+                <h2>No posts matching {searchTerm}</h2>
+                <button
+                    type="button"
+                    onClick={() => dispatch(setSearchTerm(""))}>
+                    Go Home
+                </button>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            {posts.map((post, index) => (
+                <Post
+                    key={post.id}
+                    post={post}
+                    onToggleComments={onToggleComments(index)}
+                />
+            ))}
+        </>
+    );
+};
+
+export default Home;
