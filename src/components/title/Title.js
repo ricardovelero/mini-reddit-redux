fc136ee51abf5b712b86e3875d99923365a9e473
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-02 23:14:22 +0200

    Center title

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Title = () => {
     return (
         <div className="ml-7 mb-7">
-            <h1 className="text-3xl font-bold">Mini Reddit App</h1>
+            <h1 className="text-3xl font-bold text-center">Mini Reddit App</h1>
         </div>
     );
 };
