b8c7fcadd498bb8848c513b00d1a668b567bfee3
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-03 14:00:41 +0200

    Fix meta desc and change title

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -4,7 +4,9 @@ export const Title = () => {
     return (
         <div className="mx-auto mb-7 flex items-end justify-center">
             <img src="logo.png" alt="logo" width="50" />
-            <h1 className="text-3xl font-bold text-center">Mini Reddit App</h1>
+            <h1 className="text-3xl font-bold text-center">
+                Mini Reddit Browser
+            </h1>
         </div>
     );
 };
