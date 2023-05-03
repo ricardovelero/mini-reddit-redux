400fb6cd539742b2c92b050595c91dc4236e5c05
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-03 10:17:49 +0200

    Setup slices

--- src/app/subRedditSlice.js
+++ src/app/subRedditSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createSelector } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { getSubreddits } from "../api/reddit";
 
 const initialState = {
@@ -8,9 +8,37 @@ const initialState = {
 };
 
 const subRedditSlice = createSlice({
-    name: "redditPosts",
+    name: "subreddits",
     initialState,
-    reducers: {},
+    reducers: {
+        startGetSubreddits(state) {
+            state.isLoading = true;
+            state.error = false;
+        },
+        getSubredditsSuccess(state, action) {
+            state.isLoading = false;
+            state.subreddits = action.payload;
+        },
+        getSubredditsFailed(state) {
+            state.isLoading = false;
+            state.error = true;
+        },
+    },
 });
 
+export const { getSubredditsSuccess, getSubredditsFailed, startGetSubreddits } =
+    subRedditSlice.actions;
+
+export const fetchSubreddits = () => async (dispatch) => {
+    try {
+        dispatch(startGetSubreddits());
+        const subreddits = await getSubreddits();
+        dispatch(getSubredditsSuccess(subreddits));
+    } catch (e) {
+        dispatch(getSubredditsFailed());
+    }
+};
+
 export default subRedditSlice.reducer;
+
+export const selectSubreddits = (state) => state.subreddits.subreddits;
