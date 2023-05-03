bfdbb8df3f7ed31daa293d2cf33af69473246e8d
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-03 12:17:14 +0200

    Fix wrong typing

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import redditReducer from "./redditSlice";
-import subRedditSlice from "./subRedditSlice";
+import subRedditReducer from "./subRedditSlice";
 
 export const store = configureStore({
     reducer: {
         reddit: redditReducer,
-        subReddit: subRedditSlice,
+        subReddits: subRedditReducer,
     },
 });
